583322dad7e9ef9e8e585f16e24c67f2c446fd90
Anushree Bagchi <anushreebagchi4791@gmail.com>
2018-04-23 23:42:38 +0530

    Version 2.0- Removed Add Button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function clearField()
 {
     $(".addText").val('');
 }
-$(".addButton").on('click',function(){
+$(".addText").on('change',function(){
     var inputText=$(".addText").val();  //Adding Text box
     var cbox="<input type='checkbox' class='checkbox'>"; //Adding checkbox
     $('tbody').append('<tr><td><div class="lists">'+cbox+ ' <div class="text"> '+inputText+'</div> <input type="submit" class="btnDelete"  value="Delete"></div></td></tr>') //new row addition on clicking Add button
