2596a9defc28fc9deb0594732468767a2ceeb542
Anushree Bagchi <anushreebagchi4791@gmail.com>
2018-04-23 10:44:28 +0530

    Delete button functionality added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
+
 $(".addButton").on('click',function(){
-	$('tbody').append('<tr><td> <input type="text" class="inputText" > </td></tr>')
-});
+	$('tbody').append('<tr><td> <input type="text" class="inputText" > <input type="submit" class="btnDelete"  value="Delete"></td></tr>') //new row addition on clicking Add button
 
-$(".btnDelete").on('click',function(){
-	$(this).remove();
-	//$(this).attr("class").val();
+    //Delete selected row on clicking Delete button
+    $(".btnDelete").on('click',function(){
+        $(this).parent().parent().remove();
+    })
 
-});
+});  
