b49d0c37ef9f63e490c62ec02a1559f698c4fc40
Anushree Bagchi <anushreebagchi4791@gmail.com>
2018-04-23 23:24:45 +0530

    Version 1.0

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 
-
+//Clear addText function
+function clearField()
+{
+    $(".addText").val('');
+}
 $(".addButton").on('click',function(){
-	$('tbody').append('<tr><td> <input type="text" class="inputText" > <input type="submit" class="btnDelete"  value="Delete"></td></tr>') //new row addition on clicking Add button
+    var inputText=$(".addText").val();  //Adding Text box
+    var cbox="<input type='checkbox' class='checkbox'>"; //Adding checkbox
+    $('tbody').append('<tr><td><div class="lists">'+cbox+ ' <div class="text"> '+inputText+'</div> <input type="submit" class="btnDelete"  value="Delete"></div></td></tr>') //new row addition on clicking Add button
+    
+    // clear the addText field once added
+    clearField();  
 
     //Delete selected row on clicking Delete button
     $(".btnDelete").on('click',function(){
         $(this).parent().parent().remove();
     })
-
+    
+    //strike through text on clicking checkbox
+    $(".checkbox").on('click',function(){
+        $(this).parent().toggleClass('finished');
+    })
+    
 });  
