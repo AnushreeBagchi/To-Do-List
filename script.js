d4db7f701d6c22e44788948ea879875c5b40d621
Anushree <Anushree@Abhiks-MacBook-Pro-2.local>
2018-09-06 21:16:48 +0530

    Quick bug fix!

--- script.js
+++ script.js
@@ -4,22 +4,27 @@ function clearField()
 {
     $(".addText").val('');
 }
+var count=0;
 $(".addText").on('change',function(){
+    count++;
     var inputText=$(".addText").val();  //Adding Text box
-    var cbox="<input type='checkbox' class='checkbox'>"; //Adding checkbox
-    $('tbody').append('<tr><td><div class="lists">'+cbox+ ' <div class="text"> '+inputText+'</div> <input type="submit" class="btnDelete"  value="Delete"></div></td></tr>') //new row addition on clicking Add button
+    var cbox=`<input type='checkbox' class='checkbox${count}'>`; //Adding checkbox
+    var dbtn=`<input type='submit' class='btnDelete${count}'  value='Delete'>` //adding delete button
+    $('tbody').append(`<tr><td><div class='lists'> ${cbox} <div class='text'> ${inputText} </div> ${dbtn} </div></td></tr>`) //new row addition on clicking Add button
     
     // clear the addText field once added
     clearField();  
 
     //Delete selected row on clicking Delete button
-    $(".btnDelete").on('click',function(){
+    $(`.btnDelete${count}`).on('click',function(){
         $(this).parent().parent().remove();
-    })
-    
+    });
+
     //strike through text on clicking checkbox
-    $(".checkbox").on('click',function(){
+    $(`.checkbox${count}`).bind('click',function(){
         $(this).parent().toggleClass('finished');
-    })
+    });
+    
     
 });  
+
